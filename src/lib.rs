5054e166e08e63af2e20e3f0400c0591af37fa7f
sarah el kazdadi <sarah.elkazdadi@zama.ai>
2022-07-08 14:26:59 +0200

    doc: write docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
+//! This crate computes the forward and inverse Fourier transform of a given array of some size
+//! that is a small-ish power of `2`.
+//!
+//! # Example
+//!
+//! ```
+//! use binfft::{c64, init_twiddles, fwd, inv, fft_scratch};
+//! use dyn_stack::{uninit_mem_in_global, DynStack, ReborrowMut};
+//! use num_complex::ComplexFloat;
+//!
+//! const N: usize = 4;
+//!
+//! let mut mem = uninit_mem_in_global(fft_scratch(N).unwrap());
+//! let mut stack = DynStack::new(&mut mem);
+//!
+//! let mut twiddles = [c64::new(0.0, 0.0); 2 * N];
+//! init_twiddles(N, &mut twiddles);
+//!
+//! let data = [
+//!     c64::new(1.0, 0.0),
+//!     c64::new(2.0, 0.0),
+//!     c64::new(3.0, 0.0),
+//!     c64::new(4.0, 0.0),
+//! ];
+//!
+//! let mut transformed_fwd = data;
+//! fwd(&mut transformed_fwd, &twiddles, stack.rb_mut());
+//!
+//! let mut transformed_inv = transformed_fwd;
+//! inv(&mut transformed_inv, &twiddles, stack.rb_mut());
+//!
+//! for (expected, actual) in transformed_inv.iter().map(|z| z / N as f64).zip(data) {
+//!     let diff = (expected - actual).abs();
+//!     assert!(diff < 1e-9);
+//! }
+//! ```
+
 use dyn_stack::{SizeOverflow, StackReq};
 use num_complex::Complex;
 
@@ -7,8 +44,11 @@ mod x86;
 
 pub mod dif4;
 
+/// Complex type containing two `f64` values.
 #[allow(non_camel_case_types)]
 pub type c64 = Complex<f64>;
+
+/// Maximum exponent of the power of two size that we can process.
 pub const MAX_EXP: usize = 17;
 
 /// Scratch memory requirements for calling fft functions.
@@ -16,7 +56,8 @@ pub fn fft_scratch(n: usize) -> Result<StackReq, SizeOverflow> {
     StackReq::try_new_aligned::<c64>(n, 64)
 }
 
-/// Initialize twiddles. `twiddles` must be of length `2*n`.
+/// Initialize twiddles for subsequent forward and inverse Fourier transforms of size `n`.
+/// `twiddles` must be of length `2*n`.
 pub fn init_twiddles(n: usize, twiddles: &mut [c64]) {
     assert!(n.is_power_of_two());
     let i = n.trailing_zeros() as usize;
@@ -28,12 +69,12 @@ pub fn init_twiddles(n: usize, twiddles: &mut [c64]) {
     }
 }
 
-/// Execute forward fourier transform using the computed twiddles.
+/// Execute forward Fourier transform using the computed twiddles.
 pub fn fwd(data: &mut [c64], twiddles: &[c64], stack: dyn_stack::DynStack) {
     dif4::fwd(data, twiddles, stack);
 }
 
-/// Execute inverse fourier transform using the computed twiddles.
+/// Execute inverse Fourier transform using the computed twiddles.
 pub fn inv(data: &mut [c64], twiddles: &[c64], stack: dyn_stack::DynStack) {
     dif4::inv(data, twiddles, stack);
 }
